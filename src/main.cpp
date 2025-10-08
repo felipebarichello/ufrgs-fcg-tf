9ef546b99a395e1ba46212696a19f667886bc765
Felipe B <felipewbarichello@gmail.com>
2025-10-07 21:19:04 -0300

    fmt: Spacing

--- src/main.cpp
+++ src/main.cpp
@@ -97,8 +97,7 @@ Vao BuildCubeFaces();
 
 // Definimos uma estrutura que armazenará dados necessários para renderizar
 // cada objeto da cena virtual.
-struct SceneObject
-{
+struct SceneObject {
     const char*  name;        // Nome do objeto
     void*        first_index; // Índice do primeiro vértice dentro do vetor indices[] definido em BuildTriangles()
     int          num_indices; // Número de índices do objeto dentro do vetor indices[] definido em BuildTriangles()
