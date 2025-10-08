90b1b7e17953f62f32bfb9dfab7440000da3f086
Fabricio <fabricio.lorenzon@ufgrs.br>
2025-10-08 12:04:52 -0300

    refactor: Rename dpad_states to dpads for consistency and clarity in input handling

--- src/engine/input_controller.cpp
+++ src/engine/input_controller.cpp
@@ -1,7 +1,7 @@
 #include "input_controller.hpp"
 
 std::map<std::pair<int,int>, std::vector<std::function<void()>>> InputController::key_action_handler_map;
-std::vector<DPad> InputController::dpad_states;
+std::vector<DPad> InputController::dpads;
 
 InputController InputController::init(GLFWwindow *window) {
     InputController input_controller = InputController();
@@ -10,69 +10,35 @@ InputController InputController::init(GLFWwindow *window) {
     return input_controller;
 }
 
-void InputController::attach_mouse_button_handler(GLFWmousebuttonfun handler)
-{
-    glfwSetMouseButtonCallback(this->window, handler);
-}
-
-void InputController::attach_cursor_position_handler(GLFWcursorposfun handler)
-{
-    glfwSetCursorPosCallback(this->window, handler);
-}
-
-void InputController::attach_scrolling_handler(GLFWscrollfun handler)
-{
-    glfwSetScrollCallback(this->window, handler);
-}
-
 void InputController::subscribe_key_action(int key, int action, std::function<void()> function) {
     key_action_handler_map[{key,action}].push_back(function);
 }
 
+void InputController::subscribe_press_button(int key, std::function<void()> function) {
+    subscribe_key_action(key, GLFW_PRESS, function);
+}
+
+void InputController::subscribe_hold_button(int key, bool* is_down) {
+    subscribe_key_action(key, GLFW_PRESS, [is_down]() { *is_down = true; });
+    subscribe_key_action(key, GLFW_RELEASE, [is_down]() { *is_down = false; });
+}
+
 void InputController::subscribe_dpad(glm::vec2* direction, int forward_key, int backward_key, int left_key, int right_key) {
     // Store the state for this vector
-    dpad_states.push_back(DPad());
-    DPad& state = dpad_states.back();
-    int index = (int)dpad_states.size() - 1;
-    state.direction = direction;
-    state.forward_key_is_down = false;
-    state.backward_key_is_down = false;
-    state.left_key_is_down = false;
-    state.right_key_is_down = false;
+    dpads.push_back(DPad());
+    DPad& dpad = dpads.back();
+
+    dpad.direction = direction;
+    dpad.forward_key = forward_key;
+    dpad.backward_key = backward_key;
+    dpad.left_key = left_key;
+    dpad.right_key = right_key;
 
     // capture index by value to avoid dangling reference
-    subscribe_key_action(forward_key, GLFW_PRESS, [index]() { 
-        dpad_states[index].forward_key_is_down = true;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(forward_key, GLFW_RELEASE, [index]() { 
-        dpad_states[index].forward_key_is_down = false;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(backward_key, GLFW_PRESS, [index]() { 
-        dpad_states[index].backward_key_is_down = true;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(backward_key, GLFW_RELEASE, [index]() { 
-        dpad_states[index].backward_key_is_down = false;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(left_key, GLFW_PRESS, [index]() { 
-        dpad_states[index].left_key_is_down = true;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(left_key, GLFW_RELEASE, [index]() { 
-        dpad_states[index].left_key_is_down = false;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(right_key, GLFW_PRESS, [index]() { 
-        dpad_states[index].right_key_is_down = true;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
-    subscribe_key_action(right_key, GLFW_RELEASE, [index]() { 
-        dpad_states[index].right_key_is_down = false;
-        InputController::update_dpad_state(dpad_states[index]);
-    });
+    subscribe_hold_button(forward_key, &dpad.forward_key_is_down);
+    subscribe_hold_button(backward_key, &dpad.backward_key_is_down);
+    subscribe_hold_button(left_key, &dpad.left_key_is_down);
+    subscribe_hold_button(right_key, &dpad.right_key_is_down);
 }
 
 void InputController::key_callback(GLFWwindow *window, int key, int scancode, int action, int mod)
@@ -93,26 +59,41 @@ void InputController::key_callback(GLFWwindow *window, int key, int scancode, in
         function();
     }
 
+    for (DPad& dpad : dpads) {
+        update_dpad_direction(dpad);
+    }
+
 }
 
-void InputController::update_dpad_state(DPad& vector_state) {
+void InputController::update_dpad_direction(DPad& dpad) {
 
-    *(vector_state.direction) = glm::vec2(0.0f, 0.0f);
+    *(dpad.direction) = glm::vec2(0.0f, 0.0f);
 
-    if (vector_state.forward_key_is_down)
-        vector_state.direction->y += 1.0f;
-    if (vector_state.backward_key_is_down)
-        vector_state.direction->y -= 1.0f;
-    if (vector_state.left_key_is_down)
-        vector_state.direction->x -= 1.0f;
-    if (vector_state.right_key_is_down)
-        vector_state.direction->x += 1.0f;
+    if (dpad.forward_key_is_down)
+        dpad.direction->y += 1.0f;
+    if (dpad.backward_key_is_down)
+        dpad.direction->y -= 1.0f;
+    if (dpad.left_key_is_down)
+        dpad.direction->x -= 1.0f;
+    if (dpad.right_key_is_down)
+        dpad.direction->x += 1.0f;
 
     // Normalize
-    if (*(vector_state.direction) != glm::vec2(0.0f, 0.0f))
-        *(vector_state.direction) = glm::normalize(*(vector_state.direction));
+    if (*(dpad.direction) != glm::vec2(0.0f, 0.0f))
+        *(dpad.direction) = glm::normalize(*(dpad.direction));
 }
 
+void InputController::attach_mouse_button_handler(GLFWmousebuttonfun handler)
+{
+    glfwSetMouseButtonCallback(this->window, handler);
+}
 
+void InputController::attach_cursor_position_handler(GLFWcursorposfun handler)
+{
+    glfwSetCursorPosCallback(this->window, handler);
+}
 
-
+void InputController::attach_scrolling_handler(GLFWscrollfun handler)
+{
+    glfwSetScrollCallback(this->window, handler);
+}
