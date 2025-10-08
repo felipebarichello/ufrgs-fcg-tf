855ee13108b321475cb88215ff1dd24e98f2d53f
Felipe B <felipewbarichello@gmail.com>
2025-10-08 00:02:45 -0300

    refactor: Remove unused function declaration

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,6 @@ void TextRendering_ShowFramesPerSecond(GLFWwindow* window);
 // Funções callback para comunicação com o sistema operacional e interação do
 // usuário. Veja mais comentários nas definições das mesmas, abaixo.
 //void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
-void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 void CursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
