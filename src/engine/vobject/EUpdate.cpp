7c94ba899eac5e7205232e73f647be41c6cc0795
Felipe B <felipewbarichello@gmail.com>
2025-10-08 21:50:04 -0300

    feat: Auto subscribe to Start and Update

--- src/engine/vobject/EUpdate.cpp
+++ src/engine/vobject/EUpdate.cpp
@@ -1,10 +1,12 @@
 #include "EUpdate.hpp"
 #include <iostream>
+#include <functional>
+#include "../engine_controller.hpp"
 
 // TODO: Make it similar to EStart
 namespace engine {
     EUpdate::EUpdate() {
-        // TODO
+        EngineController::get_events().subscribe_update(std::bind(&EUpdate::Update, this));
     }
 
     EUpdate::~EUpdate() {
