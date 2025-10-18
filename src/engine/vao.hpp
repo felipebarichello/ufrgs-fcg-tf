317b435453f1d3f60a5538145f0da4d4cfe035aa
Felipe B <felipewbarichello@gmail.com>
2025-10-18 20:08:05 -0300

    fmt: Adapt vao library to current style

--- src/engine/vao.hpp
+++ src/engine/vao.hpp
@@ -3,7 +3,7 @@
 #include <glad/glad.h>
 
 namespace engine {
-    typedef struct Position {
+    struct Position {
         GLfloat x;
         GLfloat y;
         GLfloat z;
@@ -12,9 +12,9 @@ namespace engine {
         Position();
         Position(GLfloat x, GLfloat y, GLfloat z);
         Position(GLfloat x, GLfloat y, GLfloat z, GLfloat w);
-    } Position;
+    };
 
-    typedef struct Color {
+    struct Color {
         GLfloat r;
         GLfloat g;
         GLfloat b;
@@ -23,62 +23,59 @@ namespace engine {
         Color();
         Color(GLfloat r, GLfloat g, GLfloat b);
         Color(GLfloat r, GLfloat g, GLfloat b, GLfloat a);
-    } Color;
+    };
 
-    typedef struct Vertex {
+    struct Vertex {
         Position position;
         Color color;
 
         Vertex(Position position, Color color);
-    } Vertex;
+    };
 
     template<size_t N>
     class GeometryEditor {
-    private:
-        Position (&vertex_positions)[N];
-        Color (&vertex_colors)[N];
+        public:
+            GeometryEditor(Position (&vertex_positions)[N], Color (&vertex_colors)[N])
+                : vertex_positions(vertex_positions), vertex_colors(vertex_colors) {}
 
-    public:
-        GeometryEditor(Position (&vertex_positions)[N], Color (&vertex_colors)[N])
-            : vertex_positions(vertex_positions), vertex_colors(vertex_colors) {}
+            void set(size_t index, Vertex vertex) {
+                this->vertex_positions[index] = vertex.position;
+                this->vertex_colors[index] = vertex.color;
+            }
 
-        void set(size_t index, Vertex vertex) {
-            this->vertex_positions[index] = vertex.position;
-            this->vertex_colors[index] = vertex.color;
-        }
+        private:
+            Position (&vertex_positions)[N];
+            Color (&vertex_colors)[N];
     };
 
-    typedef class Vao {
-    private:
-        GLuint vao_id;
-        GLenum topology_mode;
-        GLsizei topology_size;
-        GLenum topology_type;
-        
-    public:
-        Vao() = default;
-        GLuint id() const;
-        Vao(GLuint vao_id, GLenum topology_mode, GLsizei topology_size, GLenum topology_type);
-        void draw() const;
+    class Vao {            
+        public:
+            Vao() = default;
+            GLuint id() const;
+            Vao(GLuint vao_id, GLenum topology_mode, GLsizei topology_size, GLenum topology_type);
+            void draw() const;
 
-    private: 
-        void bind() const;
-        void unbind() const;
+        private:
+            GLuint vao_id;
+            GLenum topology_mode;
+            GLsizei topology_size;
+            GLenum topology_type;
 
-    } Vao;
+            void bind() const;
+            void unbind() const;
+    };
 
-    typedef class VaoBuilder {
-    private:
-        GLuint vao_id;
+    class VaoBuilder {
+        public:
+            VaoBuilder();
+            ~VaoBuilder();
+            VaoBuilder& add_vbo(GLuint location, GLint number_of_dimensions, size_t buffer_size, void* data, GLenum usage_hint);
+            VaoBuilder& add_ebo(size_t buffer_size, void* data, GLenum usage_hint);
+            Vao build(GLenum topology_mode, GLsizei topology_size, GLenum topology_type);
 
-    public:
-        VaoBuilder();
-        ~VaoBuilder();
-        VaoBuilder& add_vbo(GLuint location, GLint number_of_dimensions, size_t buffer_size, void* data, GLenum usage_hint);
-        VaoBuilder& add_ebo(size_t buffer_size, void* data, GLenum usage_hint);
-        Vao build(GLenum topology_mode, GLsizei topology_size, GLenum topology_type);
+        private:
+            GLuint vao_id;
 
-    private:
-        void add_buffer(GLenum target, size_t buffer_size, void* data, GLenum usage_hint);
-    } VaoBuilder;
+            void add_buffer(GLenum target, size_t buffer_size, void* data, GLenum usage_hint);
+    };
 }
