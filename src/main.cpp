b2ef88e1b5099da7ceb57b4d392e4a511a2b6bb9
Felipe B <felipewbarichello@gmail.com>
2025-10-18 18:28:36 -0300

    refactor: Remove unused text rendering function declarations

--- src/main.cpp
+++ src/main.cpp
@@ -50,25 +50,6 @@ void LoadShader(const char* filename, GLuint shader_id); // Função utilizada p
 //GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id); // Cria um programa de GPU
 void update_free_camera_position();
 
-// Declaração de funções auxiliares para renderizar texto dentro da janela
-// OpenGL. Estas funções estão definidas no arquivo "textrendering.cpp".
-void TextRendering_Init();
-float TextRendering_LineHeight(GLFWwindow* window);
-float TextRendering_CharWidth(GLFWwindow* window);
-void TextRendering_PrintString(GLFWwindow* window, const std::string &str, float x, float y, float scale = 1.0f);
-void TextRendering_PrintMatrix(GLFWwindow* window, glm::mat4 M, float x, float y, float scale = 1.0f);
-void TextRendering_PrintVector(GLFWwindow* window, glm::vec4 v, float x, float y, float scale = 1.0f);
-void TextRendering_PrintMatrixVectorProduct(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
-void TextRendering_PrintMatrixVectorProductMoreDigits(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
-void TextRendering_PrintMatrixVectorProductDivW(GLFWwindow* window, glm::mat4 M, glm::vec4 v, float x, float y, float scale = 1.0f);
-
-// Funções abaixo renderizam como texto na janela OpenGL algumas matrizes e
-// outras informações do programa. Definidas após main().
-void TextRendering_ShowModelViewProjection(GLFWwindow* window, glm::mat4 projection, glm::mat4 view, glm::mat4 model, glm::vec4 p_model);
-void TextRendering_ShowEulerAngles(GLFWwindow* window);
-void TextRendering_ShowProjection(GLFWwindow* window);
-void TextRendering_ShowFramesPerSecond(GLFWwindow* window);
-
 // Funções callback para comunicação com o sistema operacional e interação do
 // usuário. Veja mais comentários nas definições das mesmas, abaixo.
 //void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
@@ -270,12 +251,6 @@ void update() {
     glUniformMatrix4fv(g_projection_uniform , 1 , GL_FALSE , glm::value_ptr(projection));
 
     glBindVertexArray(0);
-
-    // Pegamos um vértice com coordenadas de modelo (0.5, 0.5, 0.5, 1) e o
-    // passamos por todos os sistemas de coordenadas armazenados nas
-    // matrizes the_model, the_view, e the_projection; e escrevemos na tela
-    // as matrizes e pontos resultantes dessas transformações.
-    glm::vec4 p_model(0.5f, 0.5f, 0.5f, 1.0f);
 }
 
 void update_free_camera_position() {
