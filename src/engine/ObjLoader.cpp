e1af07f08ade4b1739838ef08377216f5997d42a
Fabricio Lorenzon <fabricioverload@gmail.com>
2025-10-20 13:08:54 -0300

    feat: Add transformation methods and update model matrix in ObjDrawable class

--- src/engine/ObjLoader.cpp
+++ src/engine/ObjLoader.cpp
@@ -18,7 +18,46 @@ void ObjDrawable::draw(GLuint program_id) {
     glUniform3fv(Ks, 1, &this->specular_reflectance[0]);
     glUniform3fv(Ka, 1, &this->enviornment_reflectance[0]);
     glUniform1f(q, this->specular_exponent);
+    this->update_model_matrix();
+    GLsizei model_uniform = glGetUniformLocation(program_id, "model");
+    glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(this->model));
     this->vao.draw();
+}
+
+void ObjDrawable::set_diffuse_reflectance(glm::vec3 reflectance) {
+    this->diffuse_reflectance = reflectance;
+}
+
+void ObjDrawable::set_specular_reflectance(glm::vec3 reflectance) {
+    this->specular_reflectance = reflectance;
+}
+
+void ObjDrawable::set_enviornment_reflectance(glm::vec3 reflectance) {
+    this->enviornment_reflectance = reflectance;
+}
+
+void ObjDrawable::set_specular_exponent(float exponent) {
+    this->specular_exponent = exponent;
+}
+
+void ObjDrawable::set_position(glm::vec3 position) {
+    this->position = position;
+}
+
+void ObjDrawable::set_rotation(float rotation_angle, glm::vec3 rotation_axis) {
+    this->rotation_angle = rotation_angle;
+    this->rotation_axis = rotation_axis;
+}
+
+void ObjDrawable::set_scale(glm::vec3 scale) {
+    this->scale = scale;
+}
+
+void ObjDrawable::update_model_matrix() {
+    using namespace engine;
+    this->model = Matrix_Translate(this->position.x, this->position.y, this->position.z) // TERCEIRO translação
+            * Matrix_Rotate(this->rotation_angle, glm::vec4(this->rotation_axis, 0.0f)) // SEGUNDO rotação
+            * Matrix_Scale(this->scale.x, this->scale.y, this->scale.z); // PRIMEIRO escala
 }
 
 Vao ObjDrawable::build_obj_vao(ObjModel* model)
--- src/engine/ObjLoader.hpp
+++ src/engine/ObjLoader.hpp
@@ -8,6 +8,7 @@
 #include "vao.hpp"
 #include "math/matrices.hpp"
 #include "Drawable.hpp"
+#include <glm/gtc/type_ptr.hpp>
 
 namespace engine {
 
@@ -33,6 +34,10 @@ namespace engine {
         void set_enviornment_reflectance(glm::vec3 reflectance);
         void set_specular_exponent(float exponent);
 
+        void set_position(glm::vec3 position);
+        void set_rotation(float rotation_angle, glm::vec3 rotation_axis);
+        void set_scale(glm::vec3 scale);
+
     private:
         engine::Vao build_obj_vao(ObjModel* model);
         engine::Vao vao;
@@ -41,6 +46,12 @@ namespace engine {
         glm::vec3 specular_reflectance{0.8f, 0.8f, 0.8f};
         glm::vec3 enviornment_reflectance{0.04f, 0.2f, 0.4f};
         float specular_exponent{32.0f};
+        void update_model_matrix();
+        glm::vec3 position{0.0f, 0.0f, 0.0f};
+        glm::vec3 rotation_axis{0.0f, 1.0f, 0.0f};
+        glm::vec3 scale{1.0f, 1.0f, 1.0f};
+        float rotation_angle{0.0f};
+        glm::mat4 model{1.0f};
     };
 
 } // namespace engine
