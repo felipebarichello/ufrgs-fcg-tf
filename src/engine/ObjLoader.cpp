f9dbe590643141194b9388ec747a11e1a7f27c1a
Fabricio Lorenzon <fabricioverload@gmail.com>
2025-10-20 18:54:48 -0300

    feat: Implement ObjDrawable class for object rendering and management

--- src/engine/ObjLoader.cpp
+++ src/engine/ObjLoader.cpp
@@ -10,63 +10,6 @@ ObjLoader::ObjLoader(const char* filename, const char* basepath, bool triangulat
     this->vao = build_obj_vao(&obj_model);
 }
 
-ObjDrawable::ObjDrawable(Vao* vao_ptr) {
-    this->vao_ptr = vao_ptr;
-}
-
-void ObjDrawable::draw(GLuint program_id) {
-    GLint Kd = glGetUniformLocation(program_id, "diffuse_reflectance");
-    GLint Ks = glGetUniformLocation(program_id, "specular_reflectance");
-    GLint Ka = glGetUniformLocation(program_id, "enviornment_reflectance");
-    GLint q  = glGetUniformLocation(program_id, "specular_exponent");
-    glUniform3fv(Kd, 1, &this->diffuse_reflectance[0]);
-    glUniform3fv(Ks, 1, &this->specular_reflectance[0]);
-    glUniform3fv(Ka, 1, &this->enviornment_reflectance[0]);
-    glUniform1f(q, this->specular_exponent);
-    this->update_model_matrix();
-    GLsizei model_uniform = glGetUniformLocation(program_id, "model");
-    glUniformMatrix4fv(model_uniform, 1, GL_FALSE, glm::value_ptr(this->model));
-    this->vao_ptr->draw();
-}
-
-void ObjDrawable::set_diffuse_reflectance(glm::vec3 reflectance) {
-    this->diffuse_reflectance = reflectance;
-}
-
-void ObjDrawable::set_specular_reflectance(glm::vec3 reflectance) {
-    this->specular_reflectance = reflectance;
-}
-
-void ObjDrawable::set_enviornment_reflectance(glm::vec3 reflectance) {
-    this->enviornment_reflectance = reflectance;
-}
-
-void ObjDrawable::set_specular_exponent(float exponent) {
-    this->specular_exponent = exponent;
-}
-
-__supress_shadow_warning
-void ObjDrawable::set_position(glm::vec3 position) {
-    this->position = position;
-}
-
-__supress_shadow_warning
-void ObjDrawable::set_rotation(float rotation_angle, glm::vec3 rotation_axis) {
-    this->rotation_angle = rotation_angle;
-    this->rotation_axis = rotation_axis;
-}
-
-__supress_shadow_warning
-void ObjDrawable::set_scale(glm::vec3 scale) {
-    this->scale = scale;
-}
-
-void ObjDrawable::update_model_matrix() {
-    this->model = Matrix_Translate(this->position.x, this->position.y, this->position.z) // TERCEIRO translação
-            * Matrix_Rotate(this->rotation_angle, glm::vec4(this->rotation_axis, 0.0f)) // SEGUNDO rotação
-            * Matrix_Scale(this->scale.x, this->scale.y, this->scale.z); // PRIMEIRO escala
-}
-
 __supress_shadow_warning
 Vao ObjLoader::build_obj_vao(ObjModel* model) {
     GLuint vertex_array_object_id;
--- src/engine/ObjLoader.hpp
+++ src/engine/ObjLoader.hpp
@@ -9,6 +9,7 @@
 #include "math/matrices.hpp"
 #include "Drawable.hpp"
 #include <glm/gtc/type_ptr.hpp>
+#include "ObjDrawable.hpp"
 
 namespace engine {
 
@@ -20,33 +21,6 @@ namespace engine {
         // Carrega um modelo OBJ do disco
         ObjModel() = default;
         ObjModel(const char* filename, const char* basepath = NULL, bool triangulate = true);
-    };
-
-    class ObjDrawable : public Drawable {
-    public:
-        ObjDrawable(engine::Vao* vao);
-        ~ObjDrawable() = default;
-        void draw(GLuint program_id) override;
-        void set_diffuse_reflectance(glm::vec3 reflectance);
-        void set_specular_reflectance(glm::vec3 reflectance);
-        void set_enviornment_reflectance(glm::vec3 reflectance);
-        void set_specular_exponent(float exponent);
-        void set_position(glm::vec3 position);
-        void set_rotation(float rotation_angle, glm::vec3 rotation_axis);
-        void set_scale(glm::vec3 scale);
-
-    private:
-        engine::Vao* vao_ptr;
-        glm::vec3 diffuse_reflectance{0.8f, 0.4f, 0.8f};
-        glm::vec3 specular_reflectance{0.8f, 0.8f, 0.8f};
-        glm::vec3 enviornment_reflectance{0.04f, 0.2f, 0.4f};
-        float specular_exponent{32.0f};
-        void update_model_matrix();
-        glm::vec3 position{0.0f, 0.0f, 0.0f};
-        glm::vec3 rotation_axis{0.0f, 1.0f, 0.0f};
-        glm::vec3 scale{1.0f, 1.0f, 1.0f};
-        float rotation_angle{0.0f};
-        glm::mat4 model{1.0f};
     };
 
     class ObjLoader {
