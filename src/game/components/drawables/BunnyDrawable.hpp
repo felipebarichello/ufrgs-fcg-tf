1d879e8d0cf740ed551b9e90d430721e1cefb8c8
Felipe Barichello <felipewbarichello@gmail.com>
2025-10-22 18:37:04 -0300

    style: Indent

--- src/game/components/drawables/BunnyDrawable.hpp
+++ src/game/components/drawables/BunnyDrawable.hpp
@@ -5,12 +5,12 @@
 #include <glm/vec3.hpp>
 
 class BunnyDrawable : public engine::Drawable {
-public:
-    BunnyDrawable();
-    void draw() override;
-private:
-    glm::vec3 diffuse_reflectance{0.8f, 0.4f, 0.8f};
-    glm::vec3 specular_reflectance{0.8f, 0.8f, 0.8f};
-    glm::vec3 enviornment_reflectance{0.04f, 0.2f, 0.4f};
-    float specular_exponent{32.0f};
+    public:
+        BunnyDrawable();
+        void draw() override;
+    private:
+        glm::vec3 diffuse_reflectance{0.8f, 0.4f, 0.8f};
+        glm::vec3 specular_reflectance{0.8f, 0.8f, 0.8f};
+        glm::vec3 enviornment_reflectance{0.04f, 0.2f, 0.4f};
+        float specular_exponent{32.0f};
 };
