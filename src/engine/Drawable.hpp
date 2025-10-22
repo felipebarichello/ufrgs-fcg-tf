2b9285880be8f7e02c3ca04ca7979f4f1a869600
Fabricio <fabricio.lorenzon@ufgrs.br>
2025-10-22 10:34:38 -0300

    refactor: Inherit Drawable class from Component

--- src/engine/Drawable.hpp
+++ src/engine/Drawable.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include "engine/vobject/Component.hpp"
 
 namespace engine {
-    class Drawable {
+    class Drawable : public Component {
     public:
         // Accept the program ID so implementations can query/set uniforms.
         virtual void draw(GLuint program_id) = 0;
