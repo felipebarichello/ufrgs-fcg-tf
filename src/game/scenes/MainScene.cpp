e9e1243e73248d2ecee781dd13e5d22b8c934c3f
Felipe Barichello <felipewbarichello@gmail.com>
2025-10-21 21:27:36 -0300

    fmt: Fabricio's formatting

--- src/game/scenes/MainScene.cpp
+++ src/game/scenes/MainScene.cpp
@@ -5,11 +5,12 @@ namespace game::scenes {
     void MainScene::hierarchy(std::vector<VObjectConfig>& root) {
         using namespace game::components;
 
-        root.push_back(
-            VObjectConfig()
-                .component(new PlayerController())
-                .component(new Controller2())
-                .component(new Controller2())
+        root.push_back(VObjectConfig()
+            .component(new PlayerController())
+            .component(new Controller2())
+            .component(new Controller2()) // TODO: That should be illegal
+            .child(VObjectConfig()
+                .component(new Controller2()))
         );
     }
 }
