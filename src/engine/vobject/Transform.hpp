e562315f3b0f469eebb1cab1586166fc411b58f9
Fabricio <fabricio.lorenzon@ufgrs.br>
2025-10-22 10:24:25 -0300

    refactor: reorganize Transform class structure and member functions

--- src/engine/vobject/Transform.hpp
+++ src/engine/vobject/Transform.hpp
@@ -13,5 +13,17 @@ namespace engine {
     class Transform : public Component {
         public:
             Transform() = default;
+            void set_position(glm::vec3 position);
+            void set_rotation(float rotation_angle, glm::vec3 rotation_axis);
+            void set_rotation(glm::vec3 euler_angles);
+            void set_scale(glm::vec3 scale);
+            void set_scale(float uniform_scale);
+        private:
+            glm::vec3 position{0.0f, 0.0f, 0.0f};
+            glm::vec3 rotation_axis{0.0f, 1.0f, 0.0f};
+            glm::vec3 scale{1.0f, 1.0f, 1.0f};
+            float rotation_angle{0.0f};
+            glm::mat4 model{1.0f};
+            void update_model_matrix();
     };
 } // namespace engine
