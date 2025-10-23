680326af3ffcfefc47862c2fbf17db530d4b005c
Felipe Barichello <felipewbarichello@gmail.com>
2025-10-23 17:28:29 -0300

    clean: Dead code

--- src/engine/vobject/components/Camera.cpp
+++ src/engine/vobject/components/Camera.cpp
@@ -29,40 +29,4 @@ namespace engine {
 
         return -m_matrix * p_matrix;
     }
-
-    CameraTransform CameraTransform::Identity() {
-        return CameraTransform(
-            Vec3(0.0f, 0.0f, 0.0f),
-            HyperVec3(
-                Vec3(1.0f, 0.0f, 0.0f),
-                Vec3(0.0f, 1.0f, 0.0f),
-                Vec3(0.0f, 0.0f, 1.0f)
-            )
-        );
-    }
-
-    __supress_shadow_warning
-    void CameraTransform::set_position(Vec3 position) {
-        this->position = position;
-    }
-
-    void CameraTransform::set_basis_from_up_view(Vec3 up, Vec3 view) {
-        Vec3 w = negate(view);
-        Vec3 u = glm::normalize(glm::cross(up, w));
-        Vec3 v = glm::cross(w, u); // No need to normalize
-
-        this->basis = HyperVec3(u, v, w);
-    }
-
-    Mat4 CameraTransform::get_matrix() {
-        Vec3& p = this->position;
-        HyperVec3& b = this->basis;
-
-        return Mat4(
-            b.x.x,  b.x.y,  b.x.z,  0.0f,
-            b.y.x,  b.y.y,  b.y.z,  0.0f,
-            b.z.x,  b.z.y,  b.z.z,  0.0f,
-            p.x,    p.y,    p.z,    1.0f
-        );
-    }
 }
--- src/engine/vobject/components/Camera.hpp
+++ src/engine/vobject/components/Camera.hpp
@@ -29,23 +29,4 @@ namespace engine {
         private:
             inline static Camera* main = nullptr;
     };
-
-    struct CameraTransform : public ITransform {
-        Vec3 position;
-        HyperVec3 basis;
-
-        static CameraTransform Identity();
-
-        CameraTransform() = default;
-
-        CameraTransform(Vec3 position, HyperVec3 basis)
-            : position(position), basis(basis) {}
-
-        void set_position(Vec3 position);
-        Mat4 get_matrix() override;
-
-        /// @brief Sets the camera vectors given an up vector and a view vector.
-        /// Assumes the vectors are orthonormal.
-        void set_basis_from_up_view(Vec3 up, Vec3 view);
-    };
 } // namespace engine
