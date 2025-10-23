824813a1b82b7239a4dc40e190c3553104a344df
Fabricio <fabricio.lorenzon@ufgrs.br>
2025-10-23 13:59:10 -0300

    refactor: Simplify Curve class by removing inheritance from Component

--- src/engine/vobject/components/Curve.hpp
+++ src/engine/vobject/components/Curve.hpp
@@ -1,16 +1,15 @@
 #pragma once
 #include "engine/math/linalg.hpp"
-#include "engine/vobject/Component.hpp"
 #include <vector>
 
 namespace engine {
-    class Curve : public Component {
+    class Curve {
         public:
             Curve(const std::vector<Vec3> pts);
-            Vec3 get_point(float t);
-            Vec3 get_tangent(float t);
         private:
             std::vector<Vec3> points;
+            Vec3 get_point(float t);
+            Vec3 get_tangent(float t);
     };
 } // namespace engine
 
