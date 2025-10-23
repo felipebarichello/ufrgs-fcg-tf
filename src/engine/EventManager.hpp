b590d363715e9a6123a2155197e16d1e79669d49
Felipe Barichello <felipewbarichello@gmail.com>
2025-10-23 03:31:58 -0300

    clean: Remove unused event

--- src/engine/EventManager.hpp
+++ src/engine/EventManager.hpp
@@ -3,20 +3,17 @@
 #include <vector>
 #include <functional>
 
+// TODO: Eliminate
 namespace engine {
-    using StartHandler  = std::function<void()>;
     using UpdateHandler = std::function<void()>;
 
     // TODO: Restrict access modifiers?
     class EventManager {
         private:
-            std::vector<StartHandler>  start_subscribers;
             std::vector<UpdateHandler> update_subscribers;
-            bool calling_start_handlers = false;
 
         public:
             EventManager() = default;
-            void subscribe_start(StartHandler handler);
             void subscribe_update(UpdateHandler handler);
             void update();
     };
