1c3eb138b064f5799bc7c81b7783c9e38f5da1c7
Fabricio Lorenzon <fabricioverload@gmail.com>
2025-10-23 22:38:19 -0300

    feat: Update shaders to  use Gouraud Shading

--- src/engine/shaders/shader_vertex.glsl
+++ src/engine/shaders/shader_vertex.glsl
@@ -5,55 +5,48 @@
 layout (location = 0) in vec4 model_coefficients;
 layout (location = 1) in vec4 normal_coefficients;
 layout (location = 2) in vec2 texture_coefficients;
+layout (location = 3) in vec4 position;
 
 // Matrizes computadas no código C++ e enviadas para a GPU
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-// Atributos de vértice que serão gerados como saída ("out") pelo Vertex Shader.
-// ** Estes serão interpolados pelo rasterizador! ** gerando, assim, valores
-// para cada fragmento, os quais serão recebidos como entrada pelo Fragment
-// Shader. Veja o arquivo "shader_fragment.glsl".
-out vec4 position_world;
-out vec4 normal;
+uniform vec3 diffuse_reflectance;
+uniform vec3 specular_reflectance;
+uniform vec3 enviornment_reflectance;
+uniform float specular_exponent;
+
+out vec4 interpolated_color;
+
+float max(float a, float b)
+{
+    return (a > b) ? a : b;
+}
 
 void main()
 {
-    // A variável gl_Position define a posição final de cada vértice
-    // OBRIGATORIAMENTE em "normalized device coordinates" (NDC), onde cada
-    // coeficiente estará entre -1 e 1 após divisão por w.
-    // Veja {+NDC2+}.
-    //
-    // O código em "main.cpp" define os vértices dos modelos em coordenadas
-    // locais de cada modelo (array model_coefficients). Abaixo, utilizamos
-    // operações de modelagem, definição da câmera, e projeção, para computar
-    // as coordenadas finais em NDC (variável gl_Position). Após a execução
-    // deste Vertex Shader, a placa de vídeo (GPU) fará a divisão por W. Veja
-    // slides 41-67 e 69-86 do documento Aula_09_Projecoes.pdf.
-
     gl_Position = projection * view * model * model_coefficients;
 
-    // Como as variáveis acima  (tipo vec4) são vetores com 4 coeficientes,
-    // também é possível acessar e modificar cada coeficiente de maneira
-    // independente. Esses são indexados pelos nomes x, y, z, e w (nessa
-    // ordem, isto é, 'x' é o primeiro coeficiente, 'y' é o segundo, ...):
-    //
-    //     gl_Position.x = model_coefficients.x;
-    //     gl_Position.y = model_coefficients.y;
-    //     gl_Position.z = model_coefficients.z;
-    //     gl_Position.w = model_coefficients.w;
-    //
-
-    // Agora definimos outros atributos dos vértices que serão interpolados pelo
-    // rasterizador para gerar atributos únicos para cada fragmento gerado.
-
-    // Posição do vértice atual no sistema de coordenadas global (World).
-    position_world = model * model_coefficients;
-
-    // Normal do vértice atual no sistema de coordenadas global (World).
-    // Veja slides 123-151 do documento Aula_07_Transformacoes_Geometricas_3D.pdf.
-    normal = inverse(transpose(model)) * normal_coefficients;
+    vec4 normal = inverse(transpose(model)) * normal_coefficients;
     normal.w = 0.0;
+
+    vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 camera_position = inverse(view) * origin;
+
+    vec4 p = position;
+    vec4 n = normalize(normal); // normal do vertice
+    vec4 l = normalize(vec4(1.0,1.0,0.5,0.0)); // vetor da luz para o ponto
+    vec4 v = normalize(camera_position - p); // vetor do ponto para a câmera
+    vec4 r = -l + 2*n*dot(n,l); // vetor de reflexão especular ideal
+    vec4 h = normalize(l + v); // half-way entre l e v
+    vec3 I = vec3(1.0,1.0,1.0); // espectro da fonte de luz
+    vec3 Ia = vec3(0.2, 0.2, 0.2); // espectro da luz ambiente
+    vec3 lambert_diffuse_term = diffuse_reflectance*I*max(0.0, dot(n,l)); // termo difuso de Lambert
+    vec3 ambient_term = enviornment_reflectance*Ia; // termo ambiente
+    vec3 blinn_phong_specular_term  = specular_reflectance*I*pow(max(0.0, dot(n,h)), specular_exponent); // PREENCH AQUI o termo especular de Phong
+    interpolated_color.a = 1;
+    interpolated_color.rgb = lambert_diffuse_term + ambient_term + blinn_phong_specular_term;
+    interpolated_color.rgb = pow(interpolated_color.rgb, vec3(1.0,1.0,1.0)/2.2);
 }
 
