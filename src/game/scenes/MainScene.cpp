5cc9e9a20858a26d3c6a5679cbb2690768fe9bff
fabricio1739 <fabricioverload@gmail.com>
2025-11-01 13:28:35 -0300

    fix: update drawable object from bunny to moon in BunnyObj function

--- src/game/scenes/MainScene.cpp
+++ src/game/scenes/MainScene.cpp
@@ -66,7 +66,7 @@ VObjectConfig BunnyObj() {
             .position(Vec3(0.0f, 0.95f, 0.0f))
             .rotation(Quaternion::from_y_rotation(3.141592f * 1.5f))
         )
-        .component(new ObjDrawable("bunny.obj"));
+        .component(new ObjDrawable("moon.obj"));
 }
 
 VObjectConfig Enemy(HumanoidPlayerController* player_ref, std::vector<PlanetInfo*> planets) {
