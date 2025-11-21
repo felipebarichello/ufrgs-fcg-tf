13f6202b84ca2a069ece3c7df4297a4a94473264
Felipe Barichello <felipewbarichello@gmail.com>
2025-11-21 00:26:56 -0300

    refactor: ObjDrawable in namespace

--- src/engine/vobject/components/ObjDrawable.hpp
+++ src/engine/vobject/components/ObjDrawable.hpp
@@ -5,14 +5,18 @@
 #include <glm/vec3.hpp>
 #include <string>
 
-class ObjDrawable : public engine::Drawable {
-    public:
-    ObjDrawable(std::string file_name);
-    ObjDrawable(std::string file_name, engine::EngineController::ShaderType shader);
-    ObjDrawable(std::string file_name, std::string texture_file_name);
-    ObjDrawable(std::string file_name, std::string texture_file_name, engine::EngineController::ShaderType shader);
-    ~ObjDrawable();
-    void draw() override;
-    private:
-    engine::EngineController::ShaderType shader_type{ engine::EngineController::ShaderType::Planet };
-};
+namespace engine {
+    
+    class ObjDrawable : public engine::Drawable {
+        public:
+        ObjDrawable(std::string file_name);
+        ObjDrawable(std::string file_name, engine::EngineController::ShaderType shader);
+        ObjDrawable(std::string file_name, std::string texture_file_name);
+        ObjDrawable(std::string file_name, std::string texture_file_name, engine::EngineController::ShaderType shader);
+        ~ObjDrawable();
+        void draw() override;
+        private:
+        engine::EngineController::ShaderType shader_type{ engine::EngineController::ShaderType::Planet };
+    };
+
+} // namespace engine
