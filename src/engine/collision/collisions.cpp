c80a70851f72702696295e130b6b137f4452185f
Fabricio <fabricio.lorenzon@ufgrs.br>
2025-12-05 10:45:21 -0300

    refactor: remove unused sphere-sphere collision

--- src/engine/collision/collisions.cpp
+++ src/engine/collision/collisions.cpp
@@ -18,25 +18,6 @@ namespace engine::collision {
         return a + ab * t;
     }
 
-    SphereSphereCollision collide_sphere_sphere(const SphereCollider& sphere_1, const SphereCollider& sphere_2) {
-        // Get world positions from attached VObjects (if any).
-        auto vobj1 = sphere_1.get_vobject();
-        auto vobj2 = sphere_2.get_vobject();
-
-        Vec3 pos1 = vobj1->transform().get_world_position();
-        Vec3 pos2 = vobj2->transform().get_world_position();
-
-        // Vector between centers
-        Vec3 delta = pos2 - pos1;
-
-        // Compare squared distance to squared sum of radii to avoid sqrt
-        float dist2 = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
-        float rsum = sphere_1.get_radius_squared() + sphere_2.get_radius_squared();
-        bool collided = dist2 <= (rsum * rsum);
-
-        return SphereSphereCollision(collided);
-    }
-
     PointSphereCollision collide_point_sphere(const PointCollider& point, const SphereCollider& sphere) {
         auto point_vobject = point.get_vobject();
         auto sphere_vobject = sphere.get_vobject();
--- src/engine/collision/collisions.hpp
+++ src/engine/collision/collisions.hpp
@@ -6,17 +6,6 @@
 #include <engine/collision/colliders/CylinderCollider.hpp>
 
 namespace engine::collision {
-    class SphereSphereCollision {
-        public:
-            SphereSphereCollision(bool collided) : collided(collided) {}
-            bool has_collided() const { return this->collided; }
-
-        private:
-            bool collided;
-    };
-
-    SphereSphereCollision collide_sphere_sphere(const SphereCollider& sphere_1, const SphereCollider& sphere_2);
-
     class PointSphereCollision {
         public:
             PointSphereCollision(bool collided) : collided(collided) {}
